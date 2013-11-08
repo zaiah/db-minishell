90167f8b1d0322092f4bbdd3ba1ae15ff1e62719
Antonio Collins <ramar.collins@gmail.com>
2013-11-08 12:08:29 -0500

    LunchtimEe!
    Comlete thies...

--- db-minishell.sh
+++ db-minishell.sh
@@ -10,23 +10,40 @@ PROGRAM="db-minishell"
 BINDIR="$(dirname "$(readlink -f $0)")"
 SELF="$(readlink -f $0)"
 
+# Other vars
+FIRST_TERM=
+__KEY__=
+__VALUE__=
+SQLITE="/usr/bin/sqlite3"
+
 # usage() - Show usage message and die with $STATUS
+# -y | --datatypes <arg>        List the column datatypes in a table <arg>. 
 usage() {
    STATUS="${1:-0}"
    echo "Usage: ./${PROGRAM}
-	[ -  ]
+	[ - ]
 
--s | --select <arg>           desc
--f | --from <arg>             desc
--t | --to <arg>               desc
--w | --write <arg>            desc
--u | --update <arg>           desc
--t | --this <arg>             desc
--t | --that <arg>             desc
--w | --where <arg>            desc
--r | --remove                 desc
--x | --id <arg>               desc
--d | --database <arg>         desc
+-c | --columns <arg>          List the columns in a table <arg>. 
+-s | --select <arg>           Select columns from a table. 
+     --distinct <arg>         Select distinct rows from a table. 
+     --limit <arg>            Limit result set.
+     --offset <arg>           Use an offset when using the limit.
+     --having <arg>           Having ?
+     --order-by [asc|desc]    Order the rows.
+-f | --from <arg>             If \$__TABLE not set, set this to choose a
+                              table to use in a SELECT statement.
+-i | --write <arg>            Commit records in <arg> to database. 
+-u | --update <arg>           If \$__TABLE not set, set this to choose a
+                              table to use in an UPDATE statement.
+-e | --set <arg>              Set <column> = <value>
+-w | --where <arg>            Supply a clause to tune result set. 
+-r | --remove                 Remove entry or entries depending on clause. 
+     --delete                 Synonym for --remove
+-x | --remove-where <arg>     Remove entry or entries depending on <arg>.
+     --delete-where <arg>     Allows specification of clause from here.
+-z | --id <arg>               Affect an id or ids. 
+-b | --between <arg>          Affect records between range.
+-d | --database <arg>         Choose a database to work with. 
 -v | --verbose                Be verbose in output.
 -h | --help                   Show this help and quit.
 "
@@ -103,9 +120,6 @@ convert() {
 #
 # Chop a string based on positional parameters.
 #-----------------------------------------------------#
-FIRST_TERM=
-__KEY__=
-__VALUE__=
 chop_by_position() {
 	# My Term
 	__MY_TERM__="$1"
@@ -149,126 +163,104 @@ chop_by_position() {
 				break
 			fi
 		fi
-
-		# Evaluate the clause type.
-#		case "$FIRST_TERM" in
-#			'=') 	__KEY__="$(printf "$__KEY__" | sed 's/=/ = /')";; 
-#			'>') 	__KEY__="$(printf "$__KEY__" | sed 's/>/ > /')";;
-#			'<')  __KEY__="$(printf "$__KEY__" | sed 's/</ < /')";;
-#			'~')  __KEY__="$(printf "$__KEY__" | sed 's/~/ LIKE /')";;
-#			'!=') __KEY__="$(printf "NOT $__KEY__" | sed 's/!=/ = /')";;
-#			'!~') __KEY__="$(printf "NOT $__KEY__" | sed 's/!~/ LIKE /')";;
-#		esac
 	done
+
+	# Evaluate the clause type.
+	case "$FIRST_TERM" in
+		'=') 	__KEY__="$(printf "$__KEY__" | sed 's/=/ =/')";; 
+		'>') 	__KEY__="$(printf "$__KEY__" | sed 's/>/ >/')";;
+		'<')  __KEY__="$(printf "$__KEY__" | sed 's/</ </')";;
+		'~')  __KEY__="$(printf "$__KEY__" | sed 's/~/ LIKE/')";;
+		'!=') __KEY__="$(printf "NOT $__KEY__" | sed 's/!=/ =/')";;
+		'!~') __KEY__="$(printf "NOT $__KEY__" | sed 's/!~/ LIKE/')";;
+	esac
 }
 
 
-# Die if no arguments received.
-[ -z "$BASH_ARGV" ] && printf "Nothing to do\n" && usage 1
+#-----------------------------------------------------#
+# assemble_set() 
+#
+# Assemble a SET clause. 
+#-----------------------------------------------------#
+assemble_set() {
+	# My Term
+	__SETTERM__=
+	__MYSET__="$SET"
 
+	# Get rid of backslashes.
+	[[ "$__MYSET__" =~ '\' ]] && \
+		__MYSET__="$(echo "$__MYSET__" | sed 's/\\//')"
 
-# Array
-declare -a WHERE_CLAUSE 
-declare -a NOT_CLAUSE 
-
-
-# Process options.
-while [ $# -gt 0 ]
-do
-   case "$1" in
-     -s|--select)
-         DO_SELECT=true
-         shift
-         SELECT="$1"
-      ;;
-     -f|--from)
-         DO_FROM=true
-         shift
-         __TABLE="$1"
-      ;;
-     -t|--to)
-         DO_TO=true
-         shift
-         TO="$1"
-      ;;
-     -i|--write)
-         DO_WRITE=true
-         shift
-         WRITE="$1"
-      ;;
-     -u|--update)
-         DO_UPDATE=true
-         shift
-         UPDATE="$1"
-      ;;
-     --this)
-         DO_THIS=true
-         shift
-         THIS="$1"
-      ;;
-     --that)
-         DO_THAT=true
-         shift
-         THAT="$1"
-      ;;
-     -w|--where)
-         DO_WHERE=true
-         shift
-			if [[ "$1" =~ "|" ]]
-			then
-				printf "This argument can't have a pipe character (|)."
-				usage 1
-			fi
-			[ -z "$CLAUSE" ] && CLAUSE="$1" || CLAUSE="$CLAUSE|$1"
-     #    CLAUSE[${#CLAUSE[@]}]="$1"
-      ;;
-     -r|--remove)
-         DO_REMOVE=true
-      ;;
-	  -d|--database)
-			shift
-			DB="$1"
-		;;
-     --id)
-         DO_ID=true
-         shift
-         ID="$1"
-      ;;
-	  -l|--librarify)
-			DO_LIBRARIFY=true
-		;;
-     -v|--verbose)
-        VERBOSE=true
-      ;;
-     -h|--help)
-        usage 0
-      ;;
-     --) break;;
-     -*)
-      printf "Unknown argument received.\n";
-      usage 1
-     ;;
-     *) break;;
-   esac
-shift
-done
-
-
-# select
-if [ ! -z $DO_SELECT ]
-then
-	# Make sure that we've actually asked for a clause.
-	if [ -z "$__TABLE" ] || [ -z "$SELECT" ] 
+	# If it's one term, we can speed it up by just running a check for the |
+	if [[ "$__MYSET__" =~ '|' ]]
 	then
-		if [ -z $DO_LIBRARIFY ] 
-		then 
-			printf "Either no table or no columns "
-			printf "specified in the SELECT statement.\n" 
-			usage 1
-		fi
-	fi
+		# Using a '|' to mark the arguments, chop up our string accordingly.
 
-	# Buffer for our final STMT
+		# Create a buffer and save the first position.
+		declare -a __CHOP_SET__
+		__CHOP_SET__[0]=0	 			
+
+
+		# Loop through each character in our clause to mark break points.
+		for __C__ in $(seq 0 "${#__MYSET__}")
+		do
+			[[ ${__MYSET__:$__C__:1} == "|" ]] && \
+				__CHOP_SET__[${#__CHOP_SET__[@]}]=$__C__ 
+		done
+		__CHOP_SET__[${#__CHOP_SET__[@]}]=${#__MYSET__}
+
+
+		# Iterate through each of the clauses.
+		CC_COUNT=1
+		for __DD__ in ${__CHOP_SET__[@]}
+		do
+			# Break if we've reached the end.
+			[ $__DD__ == ${#__MYSET__} ] && break
+
+			# Get more creative...
+			if [ ! $__DD__ == 0 ] 
+			then
+				__DD__=$(( $__DD__ + 1 )) # Cut the char.
+				__EE__=$(( ${__CHOP_SET__[$CC_COUNT]} - $__DD__ ))
+				WHERE_TERM=${__MYSET__:$__DD__:$__EE__}
+			else
+				__EE__=$(( ${__CHOP_SET__[$CC_COUNT]} - $__DD__ ))
+				WHERE_TERM=${__MYSET__:$__DD__:$__EE__}
+			fi
+		
+			# Increment again.
+			CC_COUNT=$(( $CC_COUNT + 1 ))
+
+			# Chop up this particular part of the clause.
+			#case "$(chop_by_position "$WHERE_TERM")" in
+			chop_by_position "$WHERE_TERM"
+
+			# Build/append to the clause.
+			[ -z "$__SETTERM__" ] && \
+				__SETTERM__="$__KEY__ $(convert "$__VALUE__")" || \
+				__SETTERM__="$__SETTERM__, $__KEY__ $(convert "$__VALUE__")" 
+		done 
+
+	# Process one clause 
+	else
+		# Chop up the single clause. 
+		chop_by_position "$__MYSET__"
+
+		# Build/append to the clause.
+		__SETTERM__="$__KEY__ $(convert "$__VALUE__")" 
+	fi # [[ $__MYSET__ =~ '|' ]]
+
+	ST_TM="$__SETTERM__"
+}
+
+
+#-----------------------------------------------------#
+# assemble_clause() 
+#
+# Assemble a clause. 
+#-----------------------------------------------------#
+assemble_clause() {
+	# Buffer for our final text. 
 	STMT=
 
 	# Terms
@@ -312,7 +304,7 @@ then
 		__CHOP_CLAUSE__[${#__CHOP_CLAUSE__[@]}]=${#CLAUSE}
 
 
-		# Iterate through each of the terms.
+		# Iterate through each of the clauses.
 		CC_COUNT=1
 		for __DD__ in ${__CHOP_CLAUSE__[@]}
 		do
@@ -330,64 +322,12 @@ then
 				WHERE_TERM=${CLAUSE:$__DD__:$__EE__}
 			fi
 		
-			# Incremenet again.
-			# echo Clause: $WHERE_TERM
+			# Increment again.
 			CC_COUNT=$(( $CC_COUNT + 1 ))
-
-			# Get rid of backslashes.
-#			[[ "$WHERE_TERM" =~ '\' ]] && \
-#				WHERE_TERM="$(echo "$WHERE_TERM" | sed 's/\\//')"
-
-	# done (This code looks like it's done its job.
-
-			# Find the first non-alpha character in the string 
-			# Must localize every string.
-			# echo Clause length: ${#WHERE_TERM}
-			# echo Clause: $WHERE_TERM
-#			for __CHAR__ in `seq 0 ${#WHERE_TERM}`
-#			do
-#				# Characters.
-#				CHAR_1=${WHERE_TERM:$__CHAR__:1}
-#				
-#				# Loop through each and find the term.
-#				if [[ ${CHAR_1} == '=' ]] || \
-#					[[ ${CHAR_1} == '!' ]] || \
-#					[[ ${CHAR_1} == '~' ]] || \
-#					[[ ${CHAR_1} == '>' ]] || \
-#					[[ ${CHAR_1} == '<' ]]
-#				then
-#					# Smack.
-#					CHAR_2=${WHERE_TERM:$(( $__CHAR__ + 1 )):1}
-#
-#					# Get the next character if match was !
-#					# If not move on.
-#					if [[ ${CHAR_1} == '!' ]] && \
-#						( [[ ${CHAR_2} == '~' ]] || [[ ${CHAR_2} == '=' ]] )
-#					then
-#						FIRST_TERM="${CHAR_1}${CHAR_2}"
-#						__KEY__=${WHERE_TERM:0:$(( $__CHAR__ + 1 ))}
-#						__VALUE__=${WHERE_TERM:$(( $__CHAR__ + 2 )):${#WHERE_TERM}}
-#						break
-#					else	
-#						FIRST_TERM=$CHAR_1
-#						__KEY__=${WHERE_TERM:0:$(( $__CHAR__ + 1 ))}
-#						__VALUE__=${WHERE_TERM:$(( $__CHAR__ + 1 )):${#WHERE_TERM}}
-#						break
-#					fi
-#				fi
-#			done
 
 			# Chop up this particular part of the clause.
 			#case "$(chop_by_position "$WHERE_TERM")" in
 			chop_by_position "$WHERE_TERM"
-			case "$FIRST_TERM" in
-				'=') 	__KEY__="$(printf "$__KEY__" | sed 's/=/ = /')";; 
-				'>') 	__KEY__="$(printf "$__KEY__" | sed 's/>/ > /')";;
-				'<')  __KEY__="$(printf "$__KEY__" | sed 's/</ < /')";;
-				'~')  __KEY__="$(printf "$__KEY__" | sed 's/~/ LIKE /')";;
-				'!=') __KEY__="$(printf "NOT $__KEY__" | sed 's/!=/ = /')";;
-				'!~') __KEY__="$(printf "NOT $__KEY__" | sed 's/!~/ LIKE /')";;
-			esac
 
 			# Build/append to the clause.
 			[ -z "$STMT" ] && \
@@ -397,48 +337,186 @@ then
 
 	# Process one clause 
 	else
-		# ....
-		chop_by_position "$WHERE_TERM"
-		case "$FIRST_TERM" in
-			'=') 	__KEY__="$(printf "$__KEY__" | sed 's/=/ = /')";; 
-			'>') 	__KEY__="$(printf "$__KEY__" | sed 's/>/ > /')";;
-			'<')  __KEY__="$(printf "$__KEY__" | sed 's/</ < /')";;
-			'~')  __KEY__="$(printf "$__KEY__" | sed 's/~/ LIKE /')";;
-			'!=') __KEY__="$(printf "NOT $__KEY__" | sed 's/!=/ = /')";;
-			'!~') __KEY__="$(printf "NOT $__KEY__" | sed 's/!~/ LIKE /')";;
-		esac
+		# Chop up the single clause. 
+		chop_by_position "$CLAUSE"
 
 		# Build/append to the clause.
-		STMT="$(echo "$WHERE_TERM" $__KEY__ $(convert "$__VALUE__"))" 
+		STMT="WHERE $__KEY__ $(convert "$__VALUE__")" 
 	fi # [[ $CLAUSE =~ '|' ]]
 
-	echo $STMT
-	exit
-	# Consider writing something to prepare the clause (begin with space, then WHERE, and end with ';')
 
-	# Select all the records asked for.
-  # $SQLITE $DB "SELECT $SELECT FROM ${__TABLE}"
+	# Also check for ...
+	# ...LIMIT 
+	if [ ! -z $__LIM ] 
+	then
+		[ -z "$STMT" ] && STMT="LIMIT $__LIM" || STMT=" ${STMT} LIMIT $__LIM"
+
+	# ... ORDER BY
+	elif [ ! -z $__ORDER_BY ]
+	then
+		[ -z "$STMT" ] && STMT="ORDER BY $__ORDER_BY" || STMT=" ${STMT} ORDER BY $__ORDER_BY"
+
+	# ... HAVING
+	elif [ ! -z $__HAVING ]
+	then
+		[ -z "$STMT" ] && STMT="HAVING $__ORDER_BY" || STMT=" ${STMT} HAVING $__ORDER_BY"
+
+	# ... GROUP BY
+	elif [ ! -z $__GROUP_BY ]
+	then
+		[ -z "$STMT" ] && STMT="GROUP BY $__ORDER_BY" || STMT=" ${STMT} GROUP BY $__ORDER_BY"
+	
+	fi
+
+	# Prepare the clause (begin with space, then WHERE, and end with ';')
+	[ -z "$STMT" ] && STMT=';' || STMT=" ${STMT};"
+
+
+	# Clause
+}
+
+
+#-----------------------------------------------------#
+# die_on_char_recvd() 
+#
+# Die when a particular character is received.
+#-----------------------------------------------------#
+die_on_char_recvd() {
+	if [ -z "$1" ] || [ -z "$2" ]
+	then 
+		echo "Characters not received.\nExiting." >> /dev/stderr
+	fi
+
+	if [[ "$1" =~ "$2" ]]
+	then
+		printf "This argument can't receive a '$2' character."
+		exit 1
+	fi 
+}
+
+
+# Die if no arguments received.
+[ -z "$BASH_ARGV" ] && printf "Nothing to do\n" && usage 1
+
+
+# Array
+declare -a WHERE_CLAUSE 
+declare -a NOT_CLAUSE 
+
+
+# Process options.
+while [ $# -gt 0 ]
+do
+   case "$1" in
+     -c|--columns)
+         DO_GET_COLUMNS=true
+			shift
+			__TABLE="$1"
+		;;
+#     -y|--types)
+#         DO_GET_COLUMN_TYPES=true
+#			shift
+#			__TABLE="$1"
+#		;;
+     -s|--select)
+         DO_SEND_QUERY=true
+         DO_SELECT=true
+         shift
+         SELECT="$1"
+      ;;
+     -f|--from)
+         DO_FROM=true
+         shift
+         __TABLE="$1"
+      ;;
+	  -t|--into)
+			shift
+			__TABLE="$1"
+		;; 
+     -i|--write|--insert)
+         DO_SEND_QUERY=true
+         DO_WRITE=true
+         shift
+         WRITE="$1"
+      ;;
+     -u|--update)
+         DO_SEND_QUERY=true
+         DO_UPDATE=true
+         shift
+         __TABLE="$1"
+      ;;
+
+     -e|--set)
+         shift
+			if [[ "$1" =~ "|" ]]
+			then
+				printf "This argument can't have a pipe character (|)."
+				usage 1
+			fi
+			[ -z "$SET" ] && SET="$1" || SET="$SET|$1"
+      ;;
+
+     -w|--where)
+         DO_WHERE=true
+         shift
+			if [[ "$1" =~ "|" ]]
+			then
+				printf "This argument can't have a pipe character (|)."
+				usage 1
+			fi
+			[ -z "$CLAUSE" ] && CLAUSE="$1" || CLAUSE="$CLAUSE|$1"
+      ;;
+     -r|--remove|--delete)
+         DO_SEND_QUERY=true
+         DO_REMOVE=true
+      ;;
+	  -d|--database)
+			shift
+			DB="$1"
+		;;
+     --id)
+         DO_ID=true
+         shift
+         ID="$1"
+      ;;
+	  -l|--librarify)
+			DO_LIBRARIFY=true
+		;;
+     -v|--verbose)
+        VERBOSE=true
+      ;;
+     -h|--help)
+        usage 0
+      ;;
+     --) break;;
+     -*)
+      printf "Unknown argument received.\n";
+      usage 1
+     ;;
+     *) break;;
+   esac
+shift
+done
+
+
+# get a column listing 
+if [ ! -z $DO_GET_COLUMNS ]
+then
+	[ -z "${__TABLE}" ] && echo "No table to operate on!" && usage 1
+
+	# Anywhere a __TABLE is present, check the first chars and make
+	# sure they're not flags.
+ 	$SQLITE -header $DB "SELECT * FROM ${__TABLE} LIMIT 1" | \
+		head -n 1 | \
+		sed 's/|/ /g'	
 fi
 
 
-# write
-if [ ! -z $DO_WRITE ]
+# get a datatype listing 
+if [ ! -z $DO_GET_COLUMN_TYPES ]
 then
-   echo '...'
-fi
-
-
-# update
-if [ ! -z $DO_UPDATE ]
-then
-   echo '...'
-fi
-
-
-# remove
-if [ ! -z $DO_REMOVE ]
-then
-   echo '...'
+	[ -z "${__TABLE}" ] && echo "No table to operate on!" && usage 1
+ 	$SQLITE $DB ".schema ${__TABLE}"
 fi
 
 
@@ -447,3 +525,57 @@ if [ ! -z $DO_LIBRARIFY ]
 then
    echo '...'
 fi
+
+
+# Send a query onto the db.
+if [ ! -z $DO_SEND_QUERY ]
+then
+	# Make sure that we've actually asked for a clause.
+	if [ -z "$__TABLE" ] 
+	then
+		if [ -z $DO_LIBRARIFY ] 
+		then
+			[ ! -z $DO_SELECT ] && NO_STMT_SPECIFIED="SELECT" 
+			[ ! -z $DO_DELETE ] && NO_STMT_SPECIFIED="DELETE FROM" 
+			[ ! -z $DO_INSERT ] && NO_STMT_SPECIFIED="INSERT INTO" 
+			[ ! -z $DO_UPDATE ] && NO_STMT_SPECIFIED="UPDATE" 
+			printf "Either no table or no columns specified in the ${NO_STMT_SPECIFIED} statement.\n"
+			usage 1
+		fi
+	fi
+
+	# write
+	if [ ! -z $DO_WRITE ]
+	then
+ 		$SQLITE $DB ".schema ${__TABLE}"
+ 		echo $SQLITE $DB "INSERT INTO ${__TABLE} VALUES (  )"
+	fi
+
+	# By this point, this program needs to check for and craft a clause.
+	assemble_clause
+
+	# select
+	if [ ! -z $DO_SELECT ]
+	then
+		# Handle LIMIT, ORDER BY
+
+		# Select all the records asked for.
+ 		$SQLITE $DB "SELECT $SELECT FROM ${__TABLE}${STMT}"
+	fi
+
+	# update
+	if [ ! -z $DO_UPDATE ]
+	then
+		# Compound your SET statements, same rules apply as in regular statment
+		assemble_set
+ 		echo $SQLITE $DB "UPDATE ${__TABLE} SET ${ST_TM}${STMT}"
+	fi
+
+
+	# remove
+	if [ ! -z $DO_REMOVE ]
+	then
+ 		echo $SQLITE $DB "DELETE FROM ${__TABLE}${STMT}"
+	fi
+fi # END [ DO_SEND_QUERY ]
+
